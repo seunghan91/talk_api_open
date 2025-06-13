55ce73a73a4a66cfef2ede0108231c82e43abb57
Helper <helper@example.com>
2025-06-13 17:46:01 +0900

    fix: 컨트롤러 테스트 route helper 및 파라미터 수정
    - Auth 컨트롤러 테스트: api_v1_auth_* route helper 사용
    - Auth 파라미터 구조: user[phone_number] 형식으로 변경
    - 전화번호 형식: 국가코드 없는 한국 전화번호 형식(01012345678)
    - Pages 컨트롤러 테스트: root_url 사용
    
    모든 테스트 통과 확인

--- test/controllers/auth_controller_test.rb
+++ test/controllers/auth_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class AuthControllerTest < ActionDispatch::IntegrationTest
   test "should get request_code" do
-    get auth_request_code_url
+    post api_v1_auth_request_code_url, params: { user: { phone_number: "01012345678" } }, as: :json
     assert_response :success
   end
 
   test "should get verify_code" do
-    get auth_verify_code_url
+    post api_v1_auth_verify_code_url, params: { user: { phone_number: "01012345678", code: "123456" } }, as: :json
     assert_response :success
   end
 end
